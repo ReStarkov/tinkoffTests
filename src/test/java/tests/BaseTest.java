f1c945eccb36dea38ec652ab486327afb566ec81
roman <re_starkov@betboom.org>
2023-02-12 15:00:06 +0400

    added config for remote

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,5 @@
 package tests;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.AfterEach;
@@ -27,5 +28,19 @@ public class BaseTest {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteAddress", "selenoid.autotests.cloud") + "/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
